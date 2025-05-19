e42dd12315f7d211e7f40ccf8d7397f3b4a707a5
kyleY9 <146113920+kyleY9@users.noreply.github.com>
2025-05-19 13:19:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,29 @@ def get_student_schedule(student_id):
     query = "CALL Student_Schedule_Procedure(" + str(student_id) + ")"
     return execute_query(get_connection(), query)
 
+def display_student_schedule(student_id):
+    for row in get_student_schedule(int(student_id)):
+        print("\nPeriod: " + str(row[4]) + "\nCourse: " + row[1] + "\nRoom: " + row[2] + "\nTeacher: " + row[3])
+
+def display_grades
+
+def get_teacher_schedule(teacher_id):
+    query = "CALL Teacher_Schedule_Procedure(" + str(teacher_id) + ")"
+    return execute_query(get_connection(), query)
+
+
+def display_teacher_schedule(teacher_id):
+    for row in get_teacher_schedule(int(teacher_id)):
+        print("\nCourse: " + str(row[1]) + "\nRoom: " + row[2] + "\nPeriod: " + str(row[3]))
+
 
 # Main Code
-student_id = input("Please enter a student ID: ")
-for row in get_student_schedule(int(student_id)):
-    print("\nPeriod: " + str(row[4]) + "\nCourse: " + row[1] + "\nRoom: " + row[2] + "\nTeacher: " + row[3])
+choice = input("Are you logging in as a Student or Teacher? ")
+if choice.lower() == "student":
+    student_id = input("What is your student id? ")
+    display_student_schedule(student_id)
+elif choice.lower() == "teacher":
+    teacher_id = input("What is your teacher id? ")
+    display_teacher_schedule(teacher_id)
+else:
+    print("Sorry, that is not a valid option")
